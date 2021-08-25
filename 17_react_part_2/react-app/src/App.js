2564aa4a1a0738fdc2e8be5a13ab00fb066fd32d
Templars <antoine.ratat@gmail.com>
2021-08-25 19:02:27 +0800

    forecast and socialnetwork

--- 17_react_part_2/react-app/src/App.js
+++ 17_react_part_2/react-app/src/App.js
@@ -1,12 +1,16 @@
 import './App.css'
 import EffectHook from './components/EffectHook'
 import Timer from './components/Timer'
+import Forecast from './components/Forecast'
+import SocialNetwork from './components/SocialNetwork'
 
 function App() {
 	return (
 		<div className='App'>
 			<EffectHook />
 			<Timer />
+			<Forecast />
+			<SocialNetwork />
 		</div>
 	)
 }
